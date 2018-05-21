0a4a723b3d445e72583a47e9d6ace7798e1c00da
Royce Dy <rady182@me.com>
2018-05-21 17:21:06 +0800

    initial notes list

--- app/src/containers/Home.js
+++ app/src/containers/Home.js
@@ -1,14 +1,99 @@
 import React, { Component } from 'react';
+import { PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
+import * as firebase from 'firebase';
 import './Home.css';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      notes: []
+    };
+  }
+
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    try {
+      const userId = firebase.auth().currentUser.uid;
+      firebase
+        .database()
+        .ref(`notes/${userId}`)
+        .on('value', snapshot => {
+          console.log('snapshot:', snapshot.val());
+          const notes = [];
+          snapshot.forEach(childSnapshot => {
+            notes.push({
+              id: childSnapshot.key,
+              ...childSnapshot.val()
+            });
+          });
+          console.log('notes:', notes);
+          this.setState({
+            notes
+          });
+        });
+    } catch (e) {
+      alert(e);
+    }
+
+    this.setState({ isLoading: false });
+  }
+
+  renderNotesList(notes) {
+    return [{}].concat(notes).map(
+      (note, i) =>
+        i !== 0 ? (
+          <ListGroupItem
+            key={note.id}
+            href={`/notes/${note.id}`}
+            onClick={this.handleNoteClick}
+            header={note.content.trim().split('\n')[0]}
+          >
+            {'Created: ' + new Date(note.createdAt).toLocaleString()}
+          </ListGroupItem>
+        ) : (
+          <ListGroupItem
+            key="new"
+            href="/notes/new"
+            onClick={this.handleNoteClick}
+          >
+            <h4>
+              <b>{'\uFF0B'}</b> Create a new note
+            </h4>
+          </ListGroupItem>
+        )
+    );
+  }
+
+  renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+      </div>
+    );
+  }
+
+  renderNotes() {
+    return (
+      <div className="notes">
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          {!this.state.isLoading && this.renderNotesList(this.state.notes)}
+        </ListGroup>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="Home">
-        <div className="lander">
-          <h1>Scratch</h1>
-          <p>A simple note taking app</p>
-        </div>
+        {this.props.isAuthenticated ? this.renderNotes() : this.renderLander()}
       </div>
     );
   }
