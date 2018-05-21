8b674dd8a174f4ad97ea5dcfb52d395de5345e4b
Royce Dy <rady182@me.com>
2018-05-21 17:04:17 +0800

    added signup

---
+++ app/src/containers/Signup.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import {
+  HelpBlock,
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from 'react-bootstrap';
+import * as firebase from 'firebase';
+import LoaderButton from '../components/LoaderButton';
+import './Signup.css';
+
+export default class Signup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  }
+
+  validateForm() {
+    return (
+      this.state.email.length > 0 &&
+      this.state.password.length > 0 &&
+      this.state.password === this.state.confirmPassword
+    );
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    this.setState({ isLoading: true });
+    try {
+      await firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password);
+      this.props.userHasAuthenticated(true);
+      this.props.history.push('/');
+    } catch (e) {
+      alert(e.message);
+      this.setState({ isLoading: false });
+    }
+  };
+
+  render() {
+    return (
+      <div className="Signup">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <FormGroup controlId="confirmPassword" bsSize="large">
+            <ControlLabel>Confirm Password</ControlLabel>
+            <FormControl
+              value={this.state.confirmPassword}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <LoaderButton
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+            isLoading={this.state.isLoading}
+            text="Signup"
+            loadingText="Signing up…"
+          />
+        </form>
+      </div>
+    );
+  }
+}
